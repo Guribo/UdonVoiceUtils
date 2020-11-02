f32dba5750078f4faa7f7d25666bdeb6b7867b36
Guribo <guribovr@gmail.com>
2020-11-02 03:07:25 +0100

    feat: add player audio controls with example ui menu

---
+++ Scripts/BetterPlayerAudio.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Guribo.UdonBetterAudio.Scripts
+{
+    public class BetterPlayerAudio : UdonSharpBehaviour
+    {
+        [SerializeField] public LayerMask occlusionMask = 1 << 11;
+        [NonSerialized] public float OcclusionFactor = 0.66f;
+        [NonSerialized] public float ListenerDirectionality = 0.66f;
+        [NonSerialized] public float PlayerDirectionality = 0.66f;
+
+        [NonSerialized] public bool EnableVoiceLowpass;
+
+        [NonSerialized] public float TargetVoiceDistanceNear;
+        [NonSerialized] public float TargetVoiceDistanceFar;
+        [NonSerialized] public float TargetVoiceGain;
+        [NonSerialized] public float TargetVoiceVolumetricRadius;
+
+        [NonSerialized] public bool ForceAvatarSpatialAudio = true;
+        [NonSerialized] public bool AllowAvatarCustomAudioCurves = true;
+
+        [NonSerialized] public float TargetAvatarNearRadius;
+        [NonSerialized] public float TargetAvatarFarRadius;
+        [NonSerialized] public float TargetAvatarGain;
+        [NonSerialized] public float TargetAvatarVolumetricRadius;
+
+
+        private int _playerIndex = 0;
+        private int _playerCount;
+        private VRCPlayerApi[] _players = new VRCPlayerApi[1];
+        private readonly RaycastHit[] _rayHits = new RaycastHit[1];
+
+        private void LateUpdate()
+        {
+            UpdatePlayerList();
+            if (_playerCount < 2) return;
+            _playerIndex = (_playerIndex + 1) % _playerCount;
+
+            var vrcPlayerApi = _players[_playerIndex];
+            if (vrcPlayerApi == null) return;
+
+            // skip local player
+            var localPlayer = Networking.LocalPlayer;
+            if (localPlayer == null || vrcPlayerApi.playerId == localPlayer.playerId) return;
+
+            var listenerHead = localPlayer.GetBonePosition(HumanBodyBones.Head);
+            var otherPlayerHead = vrcPlayerApi.GetBonePosition(HumanBodyBones.Head);
+            var listenerToPlayer = (otherPlayerHead - listenerHead);
+
+            var direction = listenerToPlayer.normalized;
+            var distance = listenerToPlayer.magnitude;
+
+            var occlusionFactor = CalculateOcclusion(listenerHead, direction, distance);
+            var directionality = CalculateDirectionality(localPlayer, vrcPlayerApi, direction);
+
+            var rawDistanceReductionFactor = directionality * occlusionFactor;
+            var voiceDistanceFactor =
+                CalculateRangeReduction(distance, rawDistanceReductionFactor, TargetVoiceDistanceFar);
+            UpdateVoiceAudio(vrcPlayerApi, voiceDistanceFactor);
+
+            var avatarDistanceFactor =
+                CalculateRangeReduction(distance, rawDistanceReductionFactor, TargetAvatarFarRadius);
+            UpdateAvatarAudio(vrcPlayerApi, avatarDistanceFactor);
+        }
+
+        private float CalculateRangeReduction(float distance, float occlusionFactor, float maxAudibleRange)
+        {
+            if (maxAudibleRange <= 0f || Mathf.Abs(occlusionFactor - 1f) < 0.01f)
+            {
+                return 1f;
+            }
+
+            var remainingDistanceToFarRadius = maxAudibleRange - distance;
+            var playerCouldBeAudible = remainingDistanceToFarRadius > 0f;
+
+            var occlusion = 1f;
+            if (playerCouldBeAudible)
+            {
+                var postOcclusionFarDistance = remainingDistanceToFarRadius * occlusionFactor + distance;
+                occlusion = postOcclusionFarDistance / maxAudibleRange;
+            }
+
+            return occlusion;
+        }
+
+        private float CalculateDirectionality(VRCPlayerApi localPlayer, VRCPlayerApi otherPlayer,
+            Vector3 directionToPlayer)
+        {
+            var listenerForward = localPlayer.GetBoneRotation(HumanBodyBones.Head) * Vector3.forward;
+            var playerBackward = otherPlayer.GetBoneRotation(HumanBodyBones.Head) * Vector3.back;
+
+
+            var dotListener = 0.5f * (Vector3.Dot(listenerForward, directionToPlayer) + 1f);
+            var dotSource = 0.5f * (Vector3.Dot(playerBackward, directionToPlayer) + 1f);
+
+            var result = Mathf.Clamp01(dotListener + (1 - ListenerDirectionality)) +
+                         Mathf.Clamp01(dotSource + (1 - PlayerDirectionality));
+
+            return 0.5f * result;
+        }
+
+        private float CalculateOcclusion(Vector3 listenerHead, Vector3 direction, float distance)
+        {
+            var hits = Physics.RaycastNonAlloc(listenerHead,
+                direction,
+                _rayHits,
+                distance,
+                occlusionMask);
+
+            if (hits > 0)
+            {
+                return OcclusionFactor;
+            }
+
+            return 1f;
+        }
+
+        private void UpdateVoiceAudio(VRCPlayerApi vrcPlayerApi, float distanceFactor)
+        {
+            vrcPlayerApi.SetVoiceLowpass(EnableVoiceLowpass);
+
+            vrcPlayerApi.SetVoiceGain(TargetVoiceGain * distanceFactor);
+            vrcPlayerApi.SetVoiceDistanceFar(TargetVoiceDistanceFar * distanceFactor);
+            vrcPlayerApi.SetVoiceDistanceNear(TargetVoiceDistanceNear * distanceFactor);
+            vrcPlayerApi.SetVoiceVolumetricRadius(TargetVoiceVolumetricRadius);
+        }
+
+        private void UpdateAvatarAudio(VRCPlayerApi vrcPlayerApi, float occlusion)
+        {
+            vrcPlayerApi.SetAvatarAudioForceSpatial(ForceAvatarSpatialAudio);
+            vrcPlayerApi.SetAvatarAudioCustomCurve(AllowAvatarCustomAudioCurves);
+
+            vrcPlayerApi.SetAvatarAudioGain(TargetAvatarGain * occlusion);
+            vrcPlayerApi.SetAvatarAudioFarRadius(TargetAvatarFarRadius * occlusion);
+            vrcPlayerApi.SetAvatarAudioNearRadius(TargetAvatarNearRadius * occlusion);
+            vrcPlayerApi.SetAvatarAudioVolumetricRadius(TargetAvatarVolumetricRadius);
+        }
+
+        private void UpdatePlayerList()
+        {
+            _playerCount = VRCPlayerApi.GetPlayerCount();
+            if (_players == null || _players.Length < _playerCount)
+            {
+                _players = new VRCPlayerApi[_playerCount];
+            }
+
+            VRCPlayerApi.GetPlayers(_players);
+        }
+    }
+}
---
+++ Scripts/BetterPlayerAudioUiController.cs
@@ -0,0 +1,127 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Guribo.UdonBetterAudio.Scripts
+{
+    public class BetterPlayerAudioUiController : UdonSharpBehaviour
+    {
+        [SerializeField] private BetterPlayerAudio betterPlayerAudio;
+
+        [Header("General Settings")] [SerializeField]
+        private Slider sliderOcclusionFactor;
+
+        [SerializeField] private Slider sliderListenerDirectionality;
+        [SerializeField] private Slider sliderPlayerDirectionality;
+
+        [SerializeField] private Text textListenerDirectionality;
+        [SerializeField] private Text textPlayerDirectionality;
+        [SerializeField] private Text textOcclusionFactor;
+
+        [Header("Voice Settings")] [SerializeField]
+        private Slider sliderVoiceDistanceNear;
+
+        [SerializeField] private Slider sliderVoiceDistanceFar;
+        [SerializeField] private Slider sliderVoiceGain;
+        [SerializeField] private Slider sliderVoiceVolumetricRadius;
+
+        [SerializeField] private Text textVoiceDistanceNear;
+        [SerializeField] private Text textVoiceDistanceFar;
+        [SerializeField] private Text textVoiceGain;
+        [SerializeField] private Text textVoiceVolumetricRadius;
+
+        [SerializeField] private Toggle toggleVoiceLowpass;
+
+        [Header("Avatar Settings")] [SerializeField]
+        private Slider sliderAvatarDistanceNear;
+
+        [SerializeField] private Slider sliderAvatarDistanceFar;
+        [SerializeField] private Slider sliderAvatarGain;
+        [SerializeField] private Slider sliderAvatarVolumetricRadius;
+
+        [SerializeField] private Text texAvatarDistanceNear;
+        [SerializeField] private Text texAvatarDistanceFar;
+        [SerializeField] private Text texAvatarGain;
+        [SerializeField] private Text texAvatarVolumetricRadius;
+
+        [SerializeField] private Toggle toggleAvatarSpatialize;
+        [SerializeField] private Toggle toggleAvatarCustomCurve;
+
+
+        void Start()
+        {
+            if (!betterPlayerAudio)
+            {
+                Debug.LogError("Invalid betterPlayerAudio");
+            }
+
+            ResetAll();
+        }
+
+        public void OnEnable()
+        {
+            ResetAll();
+        }
+
+        public void OnDisable()
+        {
+            ResetAll();
+        }
+
+        public void OnSettingsChanged()
+        {
+            betterPlayerAudio.OcclusionFactor = sliderOcclusionFactor.value;
+            betterPlayerAudio.PlayerDirectionality = sliderPlayerDirectionality.value;
+            betterPlayerAudio.ListenerDirectionality = sliderListenerDirectionality.value;
+
+            betterPlayerAudio.TargetVoiceDistanceNear = sliderVoiceDistanceNear.value;
+            betterPlayerAudio.TargetVoiceDistanceFar = sliderVoiceDistanceFar.value;
+            betterPlayerAudio.TargetVoiceGain = sliderVoiceGain.value;
+            betterPlayerAudio.TargetVoiceVolumetricRadius = sliderVoiceVolumetricRadius.value;
+            betterPlayerAudio.EnableVoiceLowpass = toggleVoiceLowpass.isOn;
+
+            betterPlayerAudio.TargetAvatarNearRadius = sliderAvatarDistanceNear.value;
+            betterPlayerAudio.TargetAvatarFarRadius = sliderAvatarDistanceFar.value;
+            betterPlayerAudio.TargetAvatarGain = sliderAvatarGain.value;
+            betterPlayerAudio.TargetAvatarVolumetricRadius = sliderAvatarVolumetricRadius.value;
+            betterPlayerAudio.ForceAvatarSpatialAudio = toggleAvatarSpatialize.isOn;
+            betterPlayerAudio.AllowAvatarCustomAudioCurves = toggleAvatarCustomCurve.isOn;
+
+            textOcclusionFactor.text = sliderOcclusionFactor.value.ToString("F");
+            textPlayerDirectionality.text = sliderPlayerDirectionality.value.ToString("F");
+            textListenerDirectionality.text = sliderListenerDirectionality.value.ToString("F");
+
+            textVoiceDistanceNear.text = sliderVoiceDistanceNear.value.ToString("F1");
+            textVoiceDistanceFar.text = sliderVoiceDistanceFar.value.ToString("F1");
+            textVoiceGain.text = sliderVoiceGain.value.ToString("F1");
+            textVoiceVolumetricRadius.text = sliderVoiceVolumetricRadius.value.ToString("F1");
+
+            texAvatarDistanceNear.text = sliderAvatarDistanceNear.value.ToString("F1");
+            texAvatarDistanceFar.text = sliderAvatarDistanceFar.value.ToString("F1");
+            texAvatarGain.text = sliderAvatarGain.value.ToString("F1");
+            texAvatarVolumetricRadius.text = sliderAvatarVolumetricRadius.value.ToString("F1");
+        }
+
+        public void ResetAll()
+        {
+            sliderOcclusionFactor.value = 0.5f;
+            sliderListenerDirectionality.value = 0.5f;
+            sliderPlayerDirectionality.value = 0.75f;
+
+            sliderVoiceDistanceNear.value = 1f;
+            sliderVoiceDistanceFar.value = 250f;
+            sliderVoiceGain.value = 0f;
+            sliderVoiceVolumetricRadius.value = 0f;
+
+            toggleVoiceLowpass.isOn = true;
+
+            sliderAvatarDistanceNear.value = 0f;
+            sliderAvatarDistanceFar.value = 25f;
+            sliderAvatarGain.value = 10f;
+            sliderAvatarVolumetricRadius.value = 0f;
+
+            toggleAvatarSpatialize.isOn = true;
+            toggleAvatarCustomCurve.isOn = false;
+        }
+    }
+}
